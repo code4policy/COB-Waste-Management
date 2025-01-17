660a047a2d6074f9f417df8278292668a197af9a
bollo2606 <MELA26068@HOTMAIL.COM>
2025-01-16 21:51:16 -0500

    Deleted unnecessary js script in seasonal page and assigned a new class to the graphs

--- script.js
+++ script.js
@@ -136,7 +136,17 @@ document.getElementById(graphId).classList.add('active');
 event.currentTarget.classList.add('active');
 }
 
-!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();
+!function(){"use strict";window.addEventListener("message",(function(a){
+    if(void 0!==a.data["datawrapper-height"]){
+        var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])
+        for(var r=0;r<e.length;r++)
+            if(e[r].contentWindow===a.source){
+                var i=a.data["datawrapper-height"][t]+"px";
+                e[r].style.height=i
+            }
+        }
+    }))
+}();
 
     function showGraph(graphId) {
       // Hide all graphs
@@ -150,3 +160,4 @@ event.currentTarget.classList.add('active');
         console.error(`Graph with ID '${graphId}' not found.`);
       }
     }
+
