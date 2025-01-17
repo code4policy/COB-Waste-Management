5efee42e72fd487ab258614e01d045575f4b2591
bollo2606 <MELA26068@HOTMAIL.COM>
2025-01-16 21:12:56 -0500

    Moved styles in waste tracker to css and script to js

--- script.js
+++ script.js
@@ -137,3 +137,16 @@ event.currentTarget.classList.add('active');
 }
 
 !function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();
+
+    function showGraph(graphId) {
+      // Hide all graphs
+      document.querySelectorAll('.graph').forEach(graph => graph.classList.remove('active'));
+
+      // Show the selected graph
+      const selectedGraph = document.getElementById(graphId);
+      if (selectedGraph) {
+        selectedGraph.classList.add('active');
+      } else {
+        console.error(`Graph with ID '${graphId}' not found.`);
+      }
+    }
